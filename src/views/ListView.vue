d254bce07aaed99c5be07b56a1b426c414abf235
Dragutin Todorovic <dragutin.todorovic@rgc.rs>
2019-12-26 14:04:33 +0100

    Created components

---
+++ src/views/ListView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>Test</span>
+    <Navbar />
+    <List />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import List from '@/components/List.vue';
+export default {
+  components:{
+    Navbar,
+    List
+  }
+};
+</script>
+
+
